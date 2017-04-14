6f266c196cf012003d35f04ab3e63c3d5b09ecc9
mjeffery <michael.jeffery@gmail.com>
2017-04-14 13:41:11 -0400

    partial implementation of abstract route

--- src/Laz/Router/Route.php
+++ src/Laz/Router/Route.php
@@ -3,5 +3,56 @@ namespace Laz\Router;
 
 abstract class AbstractRoute {
 
-    abstract function exec(RouteRequest $request, RouteResponse $response, array $params);
+	private $method = 'ALL';
+	private $path = '';
+	private $tokens = [];
+	private $middleware = [];
+
+	public function __construct($middleware, $method, $path) {
+		$this->path = path;
+		$this-middleware = $middleware;
+
+		$this->parsePath();
+	}
+
+	public function getPath() {
+		return $this->path;
+	}
+
+	public function run($request, $response, $params) {
+		if( empty($this->middleware) ) {
+			return $this->doRoute($request, $response, $params);
+		} else {
+			$mWchain = new ArrayIterator($this->middleware);
+
+			$next = function($params) use ($request, $response, $mwChain, $next) {
+				if($mwChain->isValid()) {
+					$middleware = $this->initMiddleware( $mwChain->current() );
+					$mwChain->next();
+
+					return $middleware->run($request, $response, $next, $params);
+				} else {
+					return $this->doRoute($request, $response, $params);
+				}
+			};
+
+			$middleware = $this->initMiddleware( $mwChain->current() );
+			$mwChain->next();
+
+			return $middleware->run($request, $response, $next, $params);
+		}
+	}
+
+	public function match($request) {
+		//TODO 	
+	}
+
+	protected function parsePath() {
+		//TODO figure out the tokens etc
+	}
+
+	protected abstract function initMiddleware($middleware);	
+
+	protected abstract function doRoute($request, $response, $params);
 }
+
